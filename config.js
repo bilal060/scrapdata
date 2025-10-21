67ed19bea6cc3c84d168ae4407634e2762d017d4
bilal060 <ahmad_4al@hotmail.com>
2025-10-21 19:26:51 +0400

    Force server redeploy - fix 502 error

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     
     // API Security
-    API_KEY: 'android-notification-capture-2024-secure-key'
+    API_KEY: 'android-notification-capture-2024-secure-key-v2'
 };
